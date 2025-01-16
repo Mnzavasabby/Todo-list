b8e5c79104259a06222a0b8cdbb5b1be7d91041d
Mnzavasabby <rashidsabitina@gmail.com>
2025-01-16 04:41:56 -0800

    added fields to functionality to create new task

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,24 @@
+import { useState } from "react"
+
 export default function TodoList(){
+
+    const[todos,setTodos]=useState([{task:"my first task",completed:false},
+        {task:"my second task",completed:true}
+    ])
 
     function handleSubmit(event){
         event.preventDefault()
 
         let task=event.target.task.value
-        alert("my task", task)
+        if(!task){
+            alert("please provide valid task")
+            return
+        }
+
+        setTodos([...todos,{task:task,completed:false}])
 
         event.target.reset()
+
     }
     return(
         <div className="container my-5">
@@ -17,6 +29,25 @@ export default function TodoList(){
                     <input className="form-control me-2"  placeholder="New Task" name="task"/>
                     <button className="btn btn-outline-light" type="submit">Add</button>
                 </form>
+                {todos.map((todo,index)=>{
+                    return(
+                    <div key={index} className="rounded mt-4 p-2 d-flex" 
+                    style={{backgroundColor: todo.completed ? "#87FC68" : "LightGray"}}>
+
+                        <div className="me-auto">
+                            {todo.task}
+                        </div>
+                        <div>
+                        <i className={"h5 me-2 " + (todo.completed ? "bi bi-check-square" : "bi bi-square")}></i>   
+                        <i className="bi bi-trash h5 text-danger"></i>
+                        </div>
+
+                    </div>
+                    )
+
+                })
+            }
+
             </div>
         </div>
     )
